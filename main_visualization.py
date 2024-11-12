b33f8b2cd709dfcd36b7fc3413cb527a7ac6d037
GiecV <giacomo.vassallo00@gmail.com>
2024-11-12 14:54:46 +0100

    Renamed files

---
+++ main_visualization.py
@@ -0,0 +1,8 @@
+from utils.visualizer import Visualizer
+
+visualizer = Visualizer()
+
+paths = ['logs/6i.json', 'logs/36i.json', 'logs/23456i.json']
+
+visualizer.create_boxplots(paths, save=False)
+visualizer.save_best_networks(paths[2], show=False)
